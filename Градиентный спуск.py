82b90d8deeec56de71d516fb044f3c85d4f276b4
Uladzislau <vladkaz1999@gmail.com>
2017-10-22 19:34:46 +0800

    Update Градиентный спуск.py

--- Градиентный спуск.py
+++ Градиентный спуск.py
@@ -1,119 +1,99 @@
-import matplotlib.pyplot as plt
-from sympy import Symbol, solve
+from sympy import symbols, Symbol, sympify, solve, lambdify, diff
+from sympy.plotting import plot3d
 import numpy as np
-import matplotlib.animation as animation
 import pandas as pd
-from matplotlib import cm
 
-"""fig, ax = plt.subplots()
-
-x = np.arange(0, 2*np.pi, 0.01)
-line, = ax.plot(x, np.sin(x))
-
-
-def animate(i):
-    line.set_ydata(np.sin(x + i/10.0))  # update the data
-    return line,
+# Класс для точки x0 (протестирую его позже)
+class Point:
+    def __init__(self, x1, x2):
+        x1 = x1
+        x2 = x2
 
 
-# Init only required for blitting to give a clean slate.
-def init():
-    line.set_ydata(np.ma.array(x, mask=True))
-    return line,
+class GradientDescent:
+    def __init__(self, x1, x2, function, tol=0.1, it_max=100):
+        # sympify - функция перевода строки в выражение Sympy
+        self.func = sympify(function)
+        # создание объектов класса Symbol
+        # грубо говоря, если у нас есть выражение 'x1+x2'
+        # то для его написания и использования нужно будет писать:
+        # self.x1_sym + self.x2_sym
+        self.x1_sym = Symbol('x1')
+        self.x2_sym = Symbol('x2')
+        # присваивание численных переменных
+        self.x1_num = x1
+        self.x2_num = x2
+        # создание объекта альфа класса Symbol
+        self.alpha = Symbol('a')
+        # создаём символьный градиент дифференцировнием функции func по
+        # x1 и x2 (просто формула)
+        self.grad_sym = [diff(function, self.x1_sym), diff(function, self.x2_sym)]
+        # Создание численного градиента для начальной точки x0(x1, x2)
+        # А это уже численное значение формулы, при подстановке туда х1, х2
+        self.grad_num = [float(self.grad_sym[0].evalf(subs={self.x1_sym: x1, self.x2_sym: x2})),
+                         float(self.grad_sym[1].evalf(subs={self.x1_sym: x1, self.x2_sym: x2}))]
+        # Вычисление нормы матрицы, для проверки условия сходимости
+        self.norm = np.sqrt(np.sqrt(float(self.grad_num[0]**2) + float(self.grad_num[1]**2)))
+        # Максимальное число итерация(по умолчанию - 100)
+        self.it_max = it_max
+        # Точность, по умолчанию - 0.1
+        self.tol = tol
+        # Таблица со значениями
+        self.data = {'x1': [self.x1_num],
+                     'x2':[self.x2_num],
+                     'Градиент':[self.grad_num],
+                     'Итерация':[0],
+                     'Норма матрицы':[self.norm],
+                     'Альфа':[self.alpha]}
+        self.it = 1
+        # Погнали
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
-                              interval=25, blit=True)
-plt.show()"""
+    def gradient_descent(self):
+        tol = self.tol
+        alpha = self.alpha
+        grad_sym = [0, 0]
+        # Пока не превышено количество итераций
+        # И пока точность плоха
+        while self.it < self.it_max and self.norm > tol:
+            # x1_1 = x1_0 + alpha*grad[0], где alpha - это пока что символьное значение, grad[0] - численное
+            # x2_1 = x2_0 + alpha*grad[1], где alpha - это пока что символьное значение, grad[1] - численное
+            x1_sym = self.x1_num + self.grad_num[0] * self.alpha
+            x2_sym = self.x2_num + self.grad_num[1] * self.alpha
+            # Создание переменной опять же, чтобы не потерять форумулы
+            # grad1[0] = 6*x1_1 - 6*x2_1 + 8 = 6*(x1_0 + alpha*grad[0]) - 6*(x2_0 + alpha*grad[1]) + 8
+            # grad1[1] = 10*x2_1 - 6*x1_1 + 9 = 10*(x2_0 + alpha*grad[0]) - 6*(x1_0 + alpha*grad[0]) + 9
+            # grad1 - grad_sym - формула градиента
+            # grad - grad_num - численное значение градиента предыдущей итерации
+            grad_sym[0] = (self.grad_sym[0].subs({self.x1_sym: x1_sym, self.x2_sym: x2_sym}))
+            grad_sym[1] = self.grad_sym[1].subs({self.x1_sym: x1_sym, self.x2_sym: x2_sym})
+            # На данном шаге просто считаем альфу, как grad*grad1=0
+            alpha = solve(self.grad_num[0] * grad_sym[0] + self.grad_num[1] * grad_sym[1])[0]
+            # Подставляем численное значение альфы
+            self.x1_num = float(self.x1_num + alpha * self.grad_num[0])
+            self.x2_num = float(self.x2_num + alpha * self.grad_num[1])
+            # Подставляем численное значение х1_1, х2_1
+            self.grad_num = [float(self.grad_sym[0].evalf(subs={self.x1_sym: self.x1_num,
+                                                          self.x2_sym: self.x2_num})),
+                             float(self.grad_sym[1].evalf(subs={self.x1_sym: self.x1_num,
+                                                          self.x2_sym: self.x2_num}))]
+            # Считаем норму матрицы
+            self.norm = np.sqrt(float(self.grad_num[0] ** 2) + float(self.grad_num[1] ** 2))
 
+            self.__collect_data()
+            self.it += 1
 
-class Animation:
-    def __init__(self, x_list, y_list):
-        self.iter = 0
-        self.fig, self.ax = plt.subplots()
-        self.ax.grid(True)
-        self.x_list = x_list
-        self.y_list = y_list
-        self.line, = self.ax.plot(self.x_list, self.y_list, color='green')
-        xmin, xmax, step = np.array(x_list).min()-0.5, np.array(x_list).max()+0.5, 0.01
-        ymin, ymax, step = np.array(y_list).min()-0.5, np.array(y_list).max()+0.5, 0.01
-        x, y = np.meshgrid(np.arange(xmin, xmax + step, step), np.arange(ymin, ymax + step, step))
-        #Cюда вставляй коэффиценты функции, x1 - x, x2 - y
-        func = lambda x, y: 3 * x ** 2 + 5 * y ** 2 - 6 * x * y + 8 * x + 9 * y
-        z = func(x, y)
-        plt.pcolormesh(x, y, z, cmap=cm.coolwarm)
-        plt.colorbar()
-        plt.contour(x, y, z, colors='black')
+    def __collect_data(self):
+        self.data['x1'].append(self.x1_num)
+        self.data['x2'].append(self.x2_num)
+        self.data['Градиент'].append(self.grad_num)
+        self.data['Итерация'].append(self.it)
+        self.data['Норма матрицы'].append(self.norm)
+        self.data['Альфа'].append(self.norm)
 
-    def init(self):
-        self.line.set_ydata(np.ma.array(self.x_list, mask=True))
-        return self.line,
+    def print(self):
+        data = pd.DataFrame(self.data)
+        print(data)
 
-    def animate(self, i):
-        self.line.set_data(self.x_list[:i], self.y_list[:i])
-        return self.line,
-
-    def main_func(self):
-        print(self.x_list)
-        ani = animation.FuncAnimation(self.fig, self.animate, np.arange(1, 200), interval=1000, repeat_delay=0)
-        ani.save('Градиентный спуск_м.mp4')
-        plt.show()
-
-#grad_func1 - сюда вставляй коэффиценты градиент1
-#grad_func2 - сюда вставляй коэффиценты градиент2
-f1 = lambda x1, x2: 6 *x1 - 6 * x2 + 8
-f2 = lambda x1, x2: 10 * x2 - 6 * x1 + 9
-#f1_1 = sympify('6*x1-6*x2+8')
-
-
-def printing(df):
-    df = pd.DataFrame(df, columns=['Итерация', 'Норма матрицы', 'Градиент', 'х1', 'x2', 'alpha'])
-    #print(df.round(2))
-    df.round(3).to_csv('Градзіентны сход_М.csv')
-
-
-def gradient_descent(x1, x2, eps, m):
-    #fig = plt.plot()
-    k = 0
-    x1_num = x1
-    x2_num = x2
-    #plt.plot(x1, x2)
-    grad = (f1(x1_num,x2_num), f2(x1_num, x2_num))
-    print(grad)
-    err = np.sqrt(float(grad[0]**2) + float(grad[1]**2))
-    x_list = [x1]
-    y_list = [x2]
-    df = []
-    df.append([k, err, grad, x1, x2, 0] )
-    while k < m and err > eps:
-        alpha = Symbol('a')
-        x1_sym = x1_num + grad[0]*alpha
-        #print(x1_sym)
-        x2_sym = x2_num + grad[1]*alpha
-        #print(x2_sym)
-        #grad_func1 - сюда вставляй коэффиценты градиент1
-        #grad_func2 - сюда вставляй коэффиценты градиент2
-        grad_func1 = 6 * x1_sym - 6 * x2_sym + 8
-        grad_func2 = 10 * x2_sym - 6 * x1_sym + 9
-        alpha = solve(grad[0]*grad_func1+grad[1]*grad_func2)
-        x1_num = float(x1_num + alpha[0]*grad[0])
-        x2_num = float(x2_num + alpha[0]*grad[1])
-        grad = (f1(x1_num,x2_num), f2(x1_num, x2_num))
-        err = np.sqrt(float(grad[0]**2) + float(grad[1]**2))
-        x_list.append(x1_num)
-        y_list.append(x2_num)
-        df.append([k+1, err, grad, x1_num, x2_num, alpha[0]])
-        #print('iteration = ', k+1)
-        #print('gradient = {0:4f} {1:4f}'.format(grad[0], grad[1]))
-        #print('modul_gradient = %.2f' % err)
-        #print('x1 = %.2f' %  x1_num, ' x2 = %.2f' % x2_num)
-        #print('alpha = %.2f' % alpha[0])
-        #print(30*'=')
-        #print(30*'=')
-        k += 1
-    printing(df)
-    a = Animation(x_list, y_list)
-    a.main_func()
-
-
-gradient_descent(1, 3, 0.1, 100)
-
+a = GradientDescent(1, 3, '3*x1**2+5*x2**2-6*x1*x2+8*x1+9*x2')
+a.gradient_descent()
+a.print()
